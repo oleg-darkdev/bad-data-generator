b9b3a7d43d87377f038e8b0311a6378789b91f4a
olegtempl <linner_952@mail.ru>
2018-03-09 00:51:41 +0300

    tests

---
+++ test/test_mocha.js
@@ -0,0 +1,70 @@
+const assert = require('assert');
+const warmup = require('./src/index.js');
+
+describe('GOOD GEN ', () => {
+  it('name good', () => {
+    const  ;
+    assert.deepEqual();
+  });
+
+  it('email good', () => {
+    const  ;
+    assert.deepEqual();
+  });
+
+
+  it('phone good', () => {
+    const  ;
+    assert.deepEqual();
+  });
+
+  it('text fish has dafault 500 symbols', () => {
+    const  ;
+    assert.deepEqual();
+  });
+
+});
+
+describe('BAD GEN ', () => {
+   it('name bad - default', () => {
+    const  ;
+    assert.deepEqual();
+  });
+
+
+  it('name bad custom symbols', () => {
+    const  ;
+    assert.deepEqual();
+  });
+
+  it('email bad - default', () => {
+    const  ;
+    assert.deepEqual();
+  });
+
+  it('email bad custom symbols', () => {
+    const  ;
+    assert.deepEqual();
+  });
+
+  it('phone bad - default', () => {
+    const  ;
+    assert.deepEqual();
+  });
+
+  it('phone bad custom symbols', () => {
+    const  ;
+    assert.deepEqual();
+  });
+
+
+  it('bad text fish has default 500 symbols', () => {
+    const  ;
+    assert.deepEqual();
+  });
+
+  it('text fish for user argumetns 250 symbols', () => {
+    const  ;
+    assert.deepEqual();
+  });
+});
