239dcceecf1599b566b816fdacf6ae8cbacc022a
olegtempl <linner_952@mail.ru>
2018-03-09 18:12:05 +0300

    fix bug on text.js

--- src/lib/text.js
+++ src/lib/text.js
@@ -18,16 +18,27 @@ import textData from '../data/text_fish.js';         // data-array
 /* 
                 use default vocabulary
 */
-let text = (howSymbol = 500 ) => {
+let text = (howSymbol = 500 , badSymbols = badPattern ) => {
     // укарачивает весь текст из массива по дефолту  до 500 символов, если не передан аргумент.
-    return textData.substring(0, howSymbol); 
+    let textData = textData.substring(0, howSymbol),
+        getRandomInRange = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };; 
+    
+    return textData.replace(textData.charAt(getRandomInRange(0, textData.length)), 
+            badSymbols.charAt(getRandomInRange(0, badSymbols.length)))
 }
 /* 
               use custom vocabulary
 */
-let customText = (howSymbol = 500 ) => {
+// let customText = (howSymbol = 500 ) => {
 
-}
+// }
 
 
-export  { text, gText, customText }; 
+export  { text, gText }; //, customText
+
+
+
+
+
